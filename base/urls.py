e8c7f3e101dedc8d309a914cd785ea49d943ad4a
Courage Ganyiwa <couresma@gmail.com>
2025-10-27 22:22:32 +0200

    Initial commit: import local project

---
+++ base/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import home, room, createRoom, updateRoom, deleteRoom, loginPage, userProfile ,logoutUser, registerUser, deleteMessage, updateUser
+
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', loginPage, name='login'),
+    path('registerUser/', registerUser, name='register'),
+    path('logout/', logoutUser, name='logout'),
+    path('room/<str:pk>/', room, name='room'),
+    path('profile/<str:pk>', userProfile, name='user-profile'),
+    path('create-room/', createRoom, name='create-room'),
+    path('update-room/<str:pk>', updateRoom, name='update-room'),
+    path('delete-room/<str:pk>', deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>', deleteMessage, name='delete-message'),
+    path('update-user/', updateUser, name='update-user' ),
+]
+
