85ab860cdae542640ad7649fbbfeb318aa155d97
CourageGanyiwa <couresma@gmail.com>
2025-11-21 22:38:33 +0200

    lkatest changes

--- gym_bud_app/models.py
+++ gym_bud_app/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200,null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True)
-    avatar = models.ImageField(null=True,blank=True,upload_to='avatars/', default='avatars/default.svg')
+    avatar = models.ImageField(null=True,blank=True,upload_to='avatars/', default='https://res.cloudinary.com/du238xjtt/image/upload/v1763757297/avatar_juw0bq.svg')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
